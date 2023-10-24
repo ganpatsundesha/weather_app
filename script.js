0ef2d481dfabd336cd76db32c4b8991eb4c11d13
ganpatsundesha <ganpatsundesha47@gmail.com>
2023-10-24 18:20:23 +0530

    initial commit with 90% functionality and without css

---
+++ script.js
@@ -0,0 +1,50 @@
+const apiKey = "1a1150b67c76efc17533e48da419f46a";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const error = document.querySelector('.error');
+const weatherBox = document.querySelector('.weather-box');
+const formBox = document.querySelector("form");
+const searchBox = document.querySelector(".inputbox")
+const submitBox = document.querySelector(".submit")
+const temp = document.querySelector(".temp");
+const sky = document.querySelector(".sky");
+const cityName = document.querySelector(".city");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const country = document.querySelector(".country");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    const data = await response.json()
+
+    if (response.status == 404) {
+        error.textContent = `Please Enter Valid City Name`;
+        error.style.display = 'block'
+        weatherBox.style.display = 'none'
+    }
+    else { 
+        weatherBox.style.display = 'block'
+        temp.textContent =  `Temprature: ` + data.main.temp + `°C`;
+        cityName.textContent = `City: ` + data.name;
+        humidity.textContent = `Humidity: ` + data.main.humidity + `g/kg`;
+        wind.textContent = `Wind Speed: ` + data.wind.speed + `km/h`;
+        sky.textContent = `Sky: ` + data.weather[0].description;
+        country.textContent = ` Country: ` + data.sys.country;
+        error.style.display = 'none'
+    }
+}
+
+submitBox.addEventListener("click", (e) => {
+    if(searchBox.value == " " || searchBox.value == undefined) {
+        error.textContent = `Please Enter Valid City Name`;
+        error.style.display = 'block'
+    }
+    else {
+        checkWeather(searchBox.value);
+    }
+})
+
+formBox.addEventListener("click", (e) => {
+    e.preventDefault()
+})
